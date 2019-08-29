3f64070a2f944f14bedaf615365f8dac689fc24f
{dp7jm} <{dpjm@chef.net}>
2019-08-29 03:48:28 +0200

    separeted stack from program class

--- StackCS/Program.cs
+++ StackCS/Program.cs
@@ -3,35 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections;
 
 namespace StackCS
 {
-    class Stack
-    {
-        private ArrayList _stack = new ArrayList();
-        public void Push(object obj)
-        {
-            if(obj == null)
-            {
-                throw new InvalidOperationException("null can't be assigned to stack");
-            }
-            this._stack.Add(obj);
-
-        }
-        public object Pop()
-        {
-            object objectU = _stack[_stack.Count - 1];
-            _stack.RemoveAt(_stack.Count - 1);
-            return objectU;
-
-        }
-        public void Clear()
-        {
-            _stack.Clear();
-
-        }
-    }
     class Program
     {
         static void Main(string[] args)
---
+++ StackCS/Stack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace StackCS
+{
+    class Stack
+    {
+        private ArrayList _stack = new ArrayList();
+        public void Push(object obj)
+        {
+            if(obj == null)
+            {
+                throw new InvalidOperationException("null can't be assigned to stack");
+            }
+            this._stack.Add(obj);
+
+        }
+        public object Pop()
+        {
+            object objectU = _stack[_stack.Count - 1];
+            _stack.RemoveAt(_stack.Count - 1);
+            return objectU;
+
+        }
+        public void Clear()
+        {
+            _stack.Clear();
+
+        }
+    }
+}
